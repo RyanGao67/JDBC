a3760654b9f8726191eade365217db8b94ffaab3
RyanGao67 <tiangao0611@gmail.com>
2021-01-01 20:46:22 -0500

    a

--- src/main/java/com/shallowlightning/H2Demo.java
+++ src/main/java/com/shallowlightning/H2Demo.java
@@ -4,13 +4,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 public class H2Demo {
-    // How to use H2 database:
-    // java -cp ./bin/h2-1.4.200.jar org.h2.tools.Shell
-    // download H2 to linux, and run the above command
+    ////////////////////////////////////////////////////////// How to use H2 database:
+    // java -cp ./bin/h2-1.4.200.jar org.h2.tools.Shell   ====> embeded mode   ====> DB_URL=jdbc:h2:~/test/test2
+    // java -cp ./bin/h2-1.4.200.jar org.h2.tools.Server -webAllowOthers -tcpAllowOthers  ====> server mode  ====> DB_URL=jdbc:h2:tcp://localhost/~/test
+    // download H2 to linux, and run either of the above commands
     // remember to put the DB_URL, '~/test/test2' is the file path
-//  static final String JDBC_DRIVER = "org.h2.Driver";
-    static final String DB_URL = "jdbc:h2:~/test/test2";               //=====> h2
-//  static final String dbUrl  = "jdbc:mysql://localhost:3306/demo";   //=====> mysql
+    //////////////////////////////////////////////////////////
+//  static final String JDBC_DRIVER = "org.h2.Driver";                 //=====> h2 driver(Deprecated)
+//  static final String DB_URL = "jdbc:h2:~/test/test2";               //=====> h2 embeded mode
+    static final String DB_URL = "jdbc:h2:tcp://localhost/~/test";     //=====> h2 server mode
+//  static final String DB_URL = "jdbc:mysql://localhost:3306/demo";   //=====> mysql
     static final String USER = "sa";
     static final String PASS = "";
     public static void main(String[] args) {
@@ -19,7 +22,7 @@ public class H2Demo {
         try {
             conn = DriverManager.getConnection(DB_URL,USER,PASS);
             stmt = conn.createStatement();
-            String sql =  "CREATE TABLE   REGISTRATION " +
+            String sql =  "CREATE TABLE   REGISTRATION1 " +
                     "(id INTEGER not NULL, " +
                     " first VARCHAR(255), " +
                     " last VARCHAR(255), " +
